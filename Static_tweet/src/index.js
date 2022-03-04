6b49d4455af471cbbb0a27c4980d3859960d8a33
tofi-124 <tofikahm@yahoo.com>
2022-03-03 23:18:00 -0500

    static simple tweet

--- Static_tweet/src/index.js
+++ Static_tweet/src/index.js
@@ -1,12 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import 'font-awesome/css/font-awesome.min.css';
 
 function Tweet() {
-  return(<>
-  <div className="tweet">hi</div>
-  </>)
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <Author />
+        <Time />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const ReplyButton = () => <i className="fa fa-reply reply-button"></i>;
+const RetweetButton = () => <i className="fa fa-retweet retweet-button"></i>;
+const LikeButton = () => <i className="fa fa-heart like-button"></i>;
+
+function Message() {
+  return <div className="message">this is a tweet</div>;
+}
+
+function Author() {
+  return (
+    <span className="author">
+      <span className="name">Tofik</span>
+      <span className="handle">@123</span>
+    </span>
+  );
+}
+
+function Time() {
+  return <span className="time">5 hours ago</span>;
+}
+
+function Avatar() {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/nothing"
+      alt="avatar"
+      className="avatar"
+    />
+  );
 }
 
 ReactDOM.render(<Tweet />, document.querySelector("#root"));
