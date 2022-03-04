5f5ebfcf0d1ea4c850986b8f5abf1f935b9c0355
tofi-124 <tofikahm@yahoo.com>
2022-03-03 22:44:59 -0500

    static tweet component

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+function Tweet() {
+  return(<>
+  <div className="tweet">hi</div>
+  </>)
+}
+
+ReactDOM.render(<Tweet />, document.querySelector("#root"));
